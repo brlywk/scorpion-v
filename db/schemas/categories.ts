7eefbc1f0438869e60a819e6eca0d4e5898f08db
Christoph Suessmeier <mail@suessmeier.net>
2024-02-13 16:08:34 +0100

    Added: Category table, adjusted expenses table

--- db/schemas/categories.ts
+++ db/schemas/categories.ts
@@ -1,12 +1,21 @@
 import { sqliteTable, text } from "drizzle-orm/sqlite-core";
 import { createInsertSchema, createSelectSchema } from "drizzle-zod";
 import { v4 as uuid4 } from "uuid";
-import { z } from "zod";
+import type { z } from "zod";
 
-export const expenses = sqliteTable("expenses", {
+// ----- Table Definition ------------------------
+export const categories = sqliteTable("categories", {
     id: text("id")
         .$defaultFn(() => uuid4())
         .primaryKey(),
     userId: text("user_id").notNull(),
     name: text("name").notNull(),
+    icon: text("icon").default("InboxIcon"),
 });
+
+// ----- Schema & Types --------------------------
+export const categoriesInsertSchema = createInsertSchema(categories);
+export const categoriesSelectSchema = createSelectSchema(categories);
+
+export type CategoryInsert = z.infer<typeof categoriesInsertSchema>;
+export type CategorySelect = z.infer<typeof categoriesSelectSchema>;
--- db/schemas/expenses.ts
+++ db/schemas/expenses.ts
@@ -2,14 +2,18 @@ import { real, sqliteTable, text } from "drizzle-orm/sqlite-core";
 import { createInsertSchema, createSelectSchema } from "drizzle-zod";
 import { v4 as uuid4 } from "uuid";
 import { z } from "zod";
-
+import { relations } from "drizzle-orm";
 import {
     type BillingCycle,
     type Currency,
     billingCycleList,
     currencyList,
 } from "../types";
+import { categories } from "./categories";
 
+const defaultCategoryId = "0f0e69d7-c031-4a17-8e1e-499b7a540934";
+
+// ----- Table Definition ------------------------
 export const expenses = sqliteTable("expenses", {
     id: text("id")
         .$defaultFn(() => uuid4())
@@ -23,10 +27,19 @@ export const expenses = sqliteTable("expenses", {
         .$type<BillingCycle>()
         .default("Monthly")
         .notNull(),
-    categoryId: text("category_id").default("TODO").notNull(),
+    categoryId: text("category_id").default(defaultCategoryId).notNull(),
     created: text("created").$defaultFn(() => new Date().toString()),
 });
 
+// ----- Relations -------------------------------
+export const expensesRelations = relations(expenses, ({ one }) => ({
+    categories: one(categories, {
+        fields: [expenses.categoryId],
+        references: [categories.id],
+    }),
+}));
+
+// ----- Schema & Types --------------------------
 // TODO: change these when it becomes necessary
 // - Also unify if both stay the same in the end
 export const expensesInsertSchema = createInsertSchema(expenses, {
